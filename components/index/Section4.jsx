509441cac215fff919baeca92347b06d068ee970
Muntathar Jabbar <montadr9988@gmail.com>
2025-05-06 23:31:03 +0300

    chore: Update button and background styles in main.css and _index.scss

--- components/index/Section4.jsx
+++ components/index/Section4.jsx
@@ -130,9 +130,9 @@ export default function Section4({ data }) {
   }, []);
 
   return (
-    <section className="section-4 mt-5 pt-4 services">
+    <section className="section-4 mt-5 pt-4 services container">
       <div className="row d-flex justify-content-center mb-5">
-        <div className="col-lg-4 col-sm-12 text-center">
+        <div className="col-lg-4 mt-md-4 col-sm-12 text-center">
           {lang ? (
             <h2
               style={{
@@ -144,7 +144,7 @@ export default function Section4({ data }) {
               }}
             >
               {" "}
-              طاقة العراق تدعم{" "}
+              القطاعات المستهدفة{" "}
             </h2>
           ) : (
             <h2
@@ -163,10 +163,10 @@ export default function Section4({ data }) {
         </div>
       </div>
       <div className="fristr row col-12 d-flex justify-content-center ps-sm-5 ms-sm-2 ps-lg-0 ms-lg-0">
-        <div className="col-lg-2 col-sm-12 text-center">
+        <div className="col-lg-4 mt-md-4 col-sm-12 text-center">
           {" "}
           <Image
-            src="/images/picdubai/1.jpg"
+            src="/images/picdubai/1.png"
             alt="government image"
             width={3000}
             height={0}
@@ -178,13 +178,14 @@ export default function Section4({ data }) {
               color: "#faa61a",
             }}
           >
-            {lang ? "الحكومة" : "Government"}
+            {lang ? "قطاع النقل و الخدمات اللوجستية" : "Government"}
           </h3>{" "}
         </div>
-        <div className="col-lg-2 col-sm-12 text-center">
+        <div className="col-lg-4 mt-md-4 col-sm-12 text-center">
+          {" "}
           <Image
-            src="/images/picdubai/2.jpg"
-            alt="uti image "
+            src="/images/picdubai/2.png"
+            alt="government image"
             width={3000}
             height={0}
             layout="responsive"
@@ -195,14 +196,14 @@ export default function Section4({ data }) {
               color: "#faa61a",
             }}
           >
-            {lang ? "الخدمات العامة والرقابة" : "Utilities & Regulatory"}
-          </h3>
+            {lang ? "قطاع البنية التحتية و الانشاءات" : "Government"}
+          </h3>{" "}
         </div>
-        <div className="col-lg-2 col-sm-12 text-center">
+        <div className="col-lg-4 mt-md-4 col-sm-12 text-center">
           {" "}
           <Image
-            src="/images/picdubai/3.jpg"
-            alt="dev image"
+            src="/images/picdubai/3.png"
+            alt="government image"
             width={3000}
             height={0}
             layout="responsive"
@@ -213,271 +214,120 @@ export default function Section4({ data }) {
               color: "#faa61a",
             }}
           >
-            {lang
-              ? "المقاولون والمهندسون والمطورون"
-              : "EPC Contractors & Developers"}
-          </h3>
-        </div>
-        <div className="col-lg-2 col-sm-12 text-center">
-          {" "}
-          <Image
-            src="/images/picdubai/4.jpg"
-            alt="system image"
-            width={3000}
-            height={0}
-            layout="responsive"
-            className="mb-4"
-          />
-          <h3
-            style={{
-              color: "#faa61a",
-            }}
-          >
-            {lang
-              ? "المقاولون الفرعيون ومنسقي البرمجة"
-              : "Sub-contractors & System Integrators"}
-          </h3>
-        </div>
-      </div>
-      <div className="secondry row col-12 d-flex justify-content-center mb-5  ">
-        <div className="col-lg-2 col-sm-12 text-center ">
-          {" "}
-          <Image
-            src="/images/picdubai/5.jpg"
-            alt="distributors image"
-            width={3000}
-            height={0}
-            layout="responsive"
-            className="mb-4"
-          />
-          <h3
-            style={{
-              color: "#faa61a",
-            }}
-          >
-            {lang
-              ? "شركات التجارة الكهربائية"
-              : "  Distributors & Electrical Trading Companies"}
+            {lang ? "قطاع الطاقة والمرافق" : "Government"}
           </h3>{" "}
         </div>
-        <div className="col-lg-2 col-sm-12 text-center">
-          <Image
-            src="/images/picdubai/6.jpg"
-            alt="wholesalers image"
-            width={3000}
-            height={0}
-            layout="responsive"
-            className="mb-4"
-          />
-          <h3
-            style={{
-              color: "#faa61a",
-            }}
-          >
-            {lang
-              ? "تجار الجملة، المصنعون، والبائعون"
-              : "Wholesalers, Manufacturers & Resellers"}
-          </h3>
-        </div>
-        <div className="col-lg-2 col-sm-12 text-center">
+        <div className="col-lg-4 mt-md-4 col-sm-12 text-center">
           {" "}
           <Image
-            src="/images/picdubai/7.jpg"
-            alt="consultants image"
+            src="/images/picdubai/4.png"
+            alt="government image"
             width={3000}
             height={0}
             layout="responsive"
-            className="mb-4"
+            className="mb-lg-4 mb-sm-0"
           />
           <h3
             style={{
               color: "#faa61a",
             }}
           >
-            {lang ? "المستشارون" : "Consultants"}
-          </h3>
+            {lang ? "قطاع التكنلوجيا والاتصالات" : "Government"}
+          </h3>{" "}
         </div>
-        <div className="col-lg-2 col-sm-12 text-center">
+        <div className="col-lg-4 mt-md-4 col-sm-12 text-center">
           {" "}
           <Image
-            src="/images/picdubai/8.jpg"
-            alt="financiers image"
+            src="/images/picdubai/5.png"
+            alt="government image"
             width={3000}
             height={0}
             layout="responsive"
-            className="mb-4"
+            className="mb-lg-4 mb-sm-0"
           />
           <h3
             style={{
               color: "#faa61a",
             }}
           >
-            {lang ? "الخبراء الماليون" : " Financiers"}
-          </h3>
+            {lang ? "ثطاع التجارة والصناعة" : "Government"}
+          </h3>{" "}
+        </div>
+        <div className="col-lg-4 mt-md-4 col-sm-12 text-center">
+          {" "}
+          <Image
+            src="/images/picdubai/6.png"
+            alt="government image"
+            width={3000}
+            height={0}
+            layout="responsive"
+            className="mb-lg-4 mb-sm-0"
+          />
+          <h3
+            style={{
+              color: "#faa61a",
+            }}
+          >
+            {lang ? "قطاع التمويل والاستثمار" : "Government"}
+          </h3>{" "}
+        </div>
+        <div className="col-lg-4 mt-md-4 col-sm-12 text-center">
+          {" "}
+          <Image
+            src="/images/picdubai/7.png"
+            alt="government image"
+            width={3000}
+            height={0}
+            layout="responsive"
+            className="mb-lg-4 mb-sm-0"
+          />
+          <h3
+            style={{
+              color: "#faa61a",
+            }}
+          >
+            {lang ? "ثطاع السياحة والخدمات" : "Government"}
+          </h3>{" "}
+        </div>
+        <div className="col-lg-4 mt-md-4 col-sm-12 text-center">
+          {" "}
+          <Image
+            src="/images/picdubai/8.png"
+            alt="government image"
+            width={3000}
+            height={0}
+            layout="responsive"
+            className="mb-lg-4 mb-sm-0"
+          />
+          <h3
+            style={{
+              color: "#faa61a",
+            }}
+          >
+            {lang ? "قطاع التغليم والتدريب" : "Government"}
+          </h3>{" "}
+        </div>
+        <div className="col-lg-4 mt-md-4 col-sm-12 text-center">
+          {" "}
+          <Image
+            src="/images/picdubai/9.png"
+            alt="government image"
+            width={3000}
+            height={0}
+            layout="responsive"
+            className="mb-lg-4 mb-sm-0"
+          />
+          <h3
+            style={{
+              color: "#faa61a",
+            }}
+          >
+            {lang ? "القطاع الحكومي والمؤسسات الدولية" : "Government"}
+          </h3>{" "}
         </div>
       </div>
     
 
-      <div className="containers  custom-container  ">
-        <div className="row d-flex justify-content-center pt-3 ">
-          <div className="tita col-lg-6 col-sm-12 text-center ">
-            {" "}
-            <h2 className="mb-4">
-              {lang ? "احصل على أفكار استراتيجية" : "Gain Strategic Insights"}{" "}
-            </h2>
-          </div>
-        </div>
-        <div className="row d-flex ">
-          <div className="col-lg-3 col-sm-6 text-center  nopadcol p-5">
-            {" "}
-            <Image
-              src="/images/magz/10th IEE  8th Report.png"
-              alt="financiers image"
-              width={3000}
-              height={0}
-              layout="responsive"
-              className="mb-lg-1 mb-sm-0"
-            />{" "}
-            <p
-              style={{
-                fontWeight: "700",
-                fontSize: "30px"
-              }}
-              className="mb-md-5 mb-sm-0"
-            >
-              {" "}
-              {lang ? (
-                " تقرير 2024    "
-              ) : (
-                <>
-                  {" Report 2024 "} <br></br>
-                  {" "}
-                </>
-              )}
-              {/* {lang ? (
-                "  تصفح تقارير وأبحاث الصناعة "
-              ) : (
-                <>
-                  {"Decarbonization in "} <br></br>
-                  {"the GCC Region"}
-                </>
-              )} */}
-            </p>
-            <Link href={"/register"} className="main-btn mt-5  mb-5 ">
-              {lang ? " تحميل" : " Download"}
-            </Link>
-          </div>
-          <div className="col-lg-3 col-sm-6 text-center  nopadcol p-5">
-            {" "}
-            <Image
-              src="/images/magz/10th IEE  9th Guide.png"
-              alt="financiers image"
-              width={3000}
-              height={0}
-              layout="responsive"
-              className="flip mb-lg-1 mb-sm-0"
-            />{" "}
-            <p
-              style={{
-                fontWeight: "700",
-                fontSize: "30px",
-              }}
-              className="mb-md-5 mb-sm-0"
-            >
-              {" "}
-              {lang ? (
-                " دليل 2024    "
-              ) : (
-                <>
-                  {" Guide 2024 "} <br></br>{" "}
-                </>
-              )}
-            </p>
-            <Link href={"/register"} className="main-btn ">
-              {lang ? " تحميل" : " Download"}
-            </Link>
-          </div>
-          <div className="col-lg-3 col-sm-6 text-center  nopadcol p-5">
-            {" "}
-            <Image
-              src="/images/magz/10th IEE  9th Report.png"
-              alt="financiers image"
-              width={3000}
-              height={0}
-              layout="responsive"
-              className=" mb-lg-1 mb-sm-0"
-            />{" "}
-           
-               <p
-              style={{
-                fontWeight: "700",
-                fontSize: "30px"
-              }}
-              className="mb-md-5 mb-sm-0"
-            >
-              {" "}
-              {lang ? (
-                " تقرير 2023    "
-              ) : (
-                <>
-                  {" Report 2023 "} <br></br>
-                  {" "}
-                </>
-              )}
-            {/* {lang ? (
-                "  مواجهة تحديات الطاقة المتجددة"
-              ) : (
-                <>
-                  {" Navigating Renewable "} <br></br>
-                  {"Energy Challenges"}
-                </>
-              )}{" "} */}
-            </p>
-              
-            
-            <Link href={"/register"} className="main-btn ">
-              {lang ? " تحميل" : " Download"}
-            </Link>
-          </div>
-          <div className="col-lg-3 col-sm-6 text-center  nopadcol p-5">
-            {" "}
-            <Image
-              src="/images/magz/10th IEE 8th Guide.png"
-              alt="financiers image"
-              width={3000}
-              height={0}
-              layout="responsive"
-              className="flip mb-lg-1 mb-sm-0"
-            />{" "}
-            <p
-              style={{
-                fontWeight: "700",
-                fontSize:"30px",
-              }}
-              className="mb-md-5 mb-sm-0"
-            >
-              {lang ? (
-                " دليل 2023    "
-              ) : (
-                <>
-                  {" Guide 2023 "} <br></br>{" "}
-                </>
-              )}{" "}
-              {/* {lang ? (
-                "  خطوة الشرق الأوسط نحو الأفضل "
-              ) : (
-                <>
-                  {" The Middle East’s Crucial Step "} <br></br>
-                  {"Towards a Sustainable Future"}
-                </>
-              )} */}
-            </p>
-            <Link href={"/register"} className="main-btn ">
-              {lang ? " تحميل" : " Download"}
-            </Link>
-          </div>
-        </div>
-      </div>
     </section>
   );
 }
