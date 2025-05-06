e14df7269f71fe5b0e855b7cc1e240e0741a890e
Alihayder0 <aliceft393@gmail.com>
2025-05-06 23:38:41 +0300

    benf done

--- components/sectors/temp.jsx
+++ components/sectors/temp.jsx
@@ -1,62 +1,17 @@
 import React from "react";
-import { useRecoilState } from "recoil";
-import { langState } from "@/atom/atom";
 
-export default function Temp({
-  title_ar,
-  title_en,
-  desc_ar1,
-  desc_ar2,
-  desc_ar3,
-  desc_ar4,
-  desc_ar5,
-  desc_ar6,
-  desc_ar7,
-  desc_en1,
-  desc_en2,
-  desc_en3,
-  desc_en4,
-  desc_en5,
-  desc_en6,
-  desc_en7,
-  lang,
-}) {
+export default function Temp({ title_ar, title_en, desc_ar, desc_en, lang }) {
   return (
-    <div
-      className="temp container"
-      style={{ lineHeight: "1.7", fontFamily: "Arial, sans-serif" }}
-    >
+    <div className="temp container" style={{ lineHeight: "1.8" }}>
       <h2 className="main-text text-center mt-5 mb-4">
         {lang ? title_ar : title_en}
       </h2>
+
       <div
-        className={`px-0 px-md-5 mb-3 mt-5 ${
-          lang ? "text-right" : "text-left"
-        }`}
-       
-      >
-        {lang ? (
-          <>
-            <p>{desc_ar1}</p>
-            <p>{desc_ar2}</p>
-            <p>{desc_ar3}</p>
-            <p>{desc_ar4}</p>
-            <p>{desc_ar5}</p>
-            <p>{desc_ar6}</p>
-            <p>{desc_ar7}</p>
-          </>
-        ) : (
-          <>
-            <p>{desc_en1}</p>
-            <p>{desc_en2}</p>
-            <p>{desc_en3}</p>
-            <p>{desc_en4}</p>
-            <p>{desc_en5}</p>
-            <p>{desc_en6}</p>
-            <p>{desc_en7}</p>
-          </>
-        )}
-      </div>
+        className={`px-0 px-md-5 mb-3 mt-5 text-center`}
+        style={{  }}
+        dangerouslySetInnerHTML={{ __html: lang ? desc_ar : desc_en }}
+      />
     </div>
   );
 }
